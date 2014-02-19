a2c41e57a8050cd2b7266dcab046cfe3c64155b2
Kerry Buckley <kerryjbuckley@gmail.com>
2014-02-19 08:46:13 +0000

    Integration spec with slimed implementation

---
+++ lib/weather_munger.rb
@@ -0,0 +1,9 @@
+class WeatherMunger
+  def initialize file_path
+    @file_path = file_path
+  end
+
+  def call
+    puts "14"
+  end
+end
---
+++ spec/integration_spec.rb
@@ -0,0 +1,12 @@
+require "weather_munger"
+
+describe WeatherMunger, "(integration)" do
+  subject { WeatherMunger.new File.expand_path("../../data/weather.dat") }
+
+  it "prints the number of the day with the smallest temperature range" do
+    $stdout = stdout = StringIO.new
+    subject.call
+    $stdout = STDOUT
+    expect(stdout.string).to eq "14\n"
+  end
+end
